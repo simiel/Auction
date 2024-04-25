b1b58f9e1bcedaff6cc515f50cc42b9b7e7e0a54
Simiel <samensah036@st.ug.edu.gh>
2024-04-25 12:22:03 +0000

    Auction Contract & Tests

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,7 @@
 const SimpleContract = artifacts.require('SimpleContract');
+const Auction = artifacts.require('Auction');
 
 module.exports = function (deployer) {
   deployer.deploy(SimpleContract);
+  deployer.deploy(Auction);
 };
